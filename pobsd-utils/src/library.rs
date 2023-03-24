5b9a6526ed854f6f670478b918ac642395c436a6
Hukadan <me@hukadan.org>
2023-03-24 10:20:41 +0100

    Publish pobsd-utils

---
+++ pobsd-utils/src/library.rs
@@ -0,0 +1,50 @@
+use pobsd_parser::{Game, Parser, ParserResult};
+use serde_derive::Serialize;
+use std::io::Write;
+use std::path::Path;
+
+#[derive(Debug, Serialize)]
+struct GamesExport {
+    count: usize,
+    games: Vec<Game>,
+}
+
+impl GamesExport {
+    pub fn new(games: Vec<Game>) -> Self {
+        Self {
+            count: games.len(),
+            games,
+        }
+    }
+}
+pub fn check(db: impl AsRef<Path>) -> Result<(), std::io::Error> {
+    let parser = Parser::default();
+    match parser.load_from_file(&db)? {
+        ParserResult::WithError(games, lines) => {
+            let message: Vec<String> = lines.into_iter().map(|x| x.to_string()).collect();
+            println!("> {} games parsed.", games.len());
+            println!("> Errors occured at lines {}.", message.join(", "));
+        }
+        ParserResult::WithoutError(games) => {
+            println!("> {} games parsed without error.", games.len());
+        }
+    }
+    Ok(())
+}
+
+pub fn export(db: impl AsRef<Path>, js: impl AsRef<Path>) -> Result<(), std::io::Error> {
+    let parser = Parser::default();
+    match parser.load_from_file(&db)? {
+        ParserResult::WithError(_, lines) => {
+            let message: Vec<String> = lines.into_iter().map(|x| x.to_string()).collect();
+            eprintln!("> Errors occured at lines {}.", message.join(", "));
+            eprintln!("> Export aborted.");
+        }
+        ParserResult::WithoutError(games) => {
+            let game_export = GamesExport::new(games);
+            let mut js = std::fs::File::create(js)?;
+            js.write_all(&serde_json::to_vec_pretty(&game_export).unwrap())?;
+        }
+    }
+    Ok(())
+}
---
+++ pobsd-utils/src/main.rs
@@ -0,0 +1,70 @@
+mod library;
+use crate::library::{check, export};
+use clap::{arg, Command};
+use pledge::pledge_promises;
+use unveil::unveil;
+extern crate serde_json;
+
+fn cli() -> Command {
+    Command::new("pobsd-utils")
+        .about("A tool to interact and manipulate the PlayOnBSD Database")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new("check")
+                .about("Check for error in the Database")
+                .arg(arg!(<DATABASE> "The Database"))
+                .arg_required_else_help(true),
+        )
+        .subcommand(
+            Command::new("export")
+                .about("Export the Database in json format")
+                .arg(arg!(<DATABASE> "The Database"))
+                .arg_required_else_help(true)
+                .arg(arg!(<JSON> "The json file"))
+                .arg_required_else_help(true),
+        )
+}
+
+fn main() -> Result<(), std::io::Error> {
+    pledge_promises![Stdio Unveil Rpath Wpath Cpath Tty]
+        .or_else(pledge::Error::ignore_platform)
+        .unwrap();
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("check", sub_matches)) => {
+            pledge_promises![Stdio Unveil Rpath]
+                .or_else(pledge::Error::ignore_platform)
+                .unwrap();
+            let db = sub_matches.get_one::<String>("DATABASE").expect("required");
+            unveil(db, "r")
+                .or_else(unveil::Error::ignore_platform)
+                .unwrap();
+            unveil("", "")
+                .or_else(unveil::Error::ignore_platform)
+                .unwrap();
+            check(db)?
+        }
+        Some(("export", sub_matches)) => {
+            pledge_promises![Stdio Unveil Rpath Wpath Cpath]
+                .or_else(pledge::Error::ignore_platform)
+                .unwrap();
+            let db = sub_matches.get_one::<String>("DATABASE").expect("required");
+            let js = sub_matches.get_one::<String>("JSON").expect("required");
+            unveil(db, "r")
+                .or_else(unveil::Error::ignore_platform)
+                .unwrap();
+            unveil(js, "cw")
+                .or_else(unveil::Error::ignore_platform)
+                .unwrap();
+            unveil("", "")
+                .or_else(unveil::Error::ignore_platform)
+                .unwrap();
+            export(db, js)?
+        }
+        _ => println!("Unsupported command"),
+    }
+    Ok(())
+}
